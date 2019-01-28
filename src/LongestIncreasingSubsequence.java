307c14f51544a3a2fac4d28871bc90b258a7d96a
komalb711 <komalbhavsar711@gmail.com>
2019-01-28 12:37:53 -0500

    Longest Increasing Subsequence

--- src/LongestIncreasingSubsequence.java
+++ src/LongestIncreasingSubsequence.java
@@ -9,6 +9,7 @@ public class LongestIncreasingSubsequence {
         LongestIncreasingSubsequence obj = new LongestIncreasingSubsequence();
         obj.getUserInputs();
         obj.simpleLIS();
+        obj.optimizedLIS();
     }
 
     public void getUserInputs() {
@@ -21,6 +22,7 @@ public class LongestIncreasingSubsequence {
     }
 
     public void simpleLIS() {
+        System.out.println("\nLIS algo - O(n^2) complexity");
         int dpArray[] = new int[numberCount];
         for (int i = 0; i < numberCount; i++) {
             dpArray[i] = 1;
@@ -42,21 +44,91 @@ public class LongestIncreasingSubsequence {
             }
         }
 
-        System.out.println("Lenght of LIS:"+ max);
+        System.out.println("Length of LIS:" + max);
 
         int currIdx = maxIdx;
         String msg = String.valueOf(numbers[currIdx]);
         for (int i = maxIdx; i >= 0; i--) {
-            if(dpArray[currIdx]-1 ==dpArray[i]){
+            if (dpArray[currIdx] - 1 == dpArray[i]) {
                 msg = numbers[i] + " " + msg;
                 currIdx = i;
             }
         }
-        System.out.println(msg);
+        System.out.println("Longest Increasing Sequence:" + msg);
     }
 
-    public void optimizedLIS(){
+    public void optimizedLIS() {
+        System.out.println("\nLIS algo - O(nlogn) complexity");
+        int[] lastElement = new int[numberCount];
+        int[] sequenceLinkedMap = new int[numberCount];
 
+        for (int i = 0; i < numberCount; i++) {
+            lastElement[i] = -1;
+            sequenceLinkedMap[i] = -1;
+        }
+        lastElement[0] = 0;
+        int len = 0;
+
+        for (int i = 1; i < numberCount; i++) {
+            if(numbers[lastElement[0]]>numbers[i]){
+                lastElement[0] = i;
+            }
+            else if(numbers[lastElement[len]]<numbers[i]){
+                len++;
+                lastElement[len] = i;
+                sequenceLinkedMap[lastElement[len]] = lastElement[len-1];
+            }
+            else{
+                int position = ceilIndex(numbers, lastElement, 0, len, numbers[i]);
+                lastElement[position] = i;
+                sequenceLinkedMap[lastElement[position]] = lastElement[position-1];
+            }
+        }
+
+        System.out.println("Length of LIS:" + (len+1));
+        findSequence(sequenceLinkedMap, lastElement[len]);
+    }
+
+    public void findSequence(int[] sequenceLinkedMap, int lastValueIndex){
+        String solution = String.valueOf(numbers[lastValueIndex]);
+        while(sequenceLinkedMap[lastValueIndex]!=-1){
+            solution = numbers[sequenceLinkedMap[lastValueIndex]] + " " + solution;
+            lastValueIndex = sequenceLinkedMap[lastValueIndex];
+        }
+        System.out.println("Longest Increasing Sequence:" + solution);
+
+    }
+
+    public int ceilIndex(int arr[], int lastElement[], int left, int right, int key) {
+        int mid;
+        while (left <= right) {
+            mid = (right + left) / 2;
+            if (arr[lastElement[mid]] < key && arr[lastElement[mid+1]] >= key) {
+                return mid + 1;
+            } else if (arr[lastElement[mid]] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int ceilIndex(int input[], int T[], int end, int s){
+        int start = 0;
+        int middle;
+        int len = end;
+        while(start <= end){
+            middle = (start + end)/2;
+            if(middle < len && input[T[middle]] < s && s <= input[T[middle+1]]){
+                return middle+1;
+            }else if(input[T[middle]] < s){
+                start = middle+1;
+            }else{
+                end = middle-1;
+            }
+        }
+        return -1;
     }
 
 
